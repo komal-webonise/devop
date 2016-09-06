69781f2da6e87715a907e24a0551cf4a87a16c13
komal-webonise <komal.lunkad@weboniselab.com>
2016-09-06 16:41:33 +0530

    code pushed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello..you are welcomed'
+    return 'Hello..you are welcomed One more line'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
